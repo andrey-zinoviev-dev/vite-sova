1339f5e58eee7758a6bd4eed0bc8e23a43a5a73a
Andrey <55049581+andrey-zinoviev-dev@users.noreply.github.com>
2025-02-01 18:16:14 +0300

    updated search component, added notification layout

--- src/Header.tsx
+++ src/Header.tsx
@@ -2,7 +2,7 @@ import { Link } from "react-router";
 import logo from "./assets/image 2.svg";
 import "./Header.css"
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faMagnifyingGlass, faUserCircle } from "@fortawesome/free-solid-svg-icons";
+import { faBell, faMagnifyingGlass, faUserCircle } from "@fortawesome/free-solid-svg-icons";
 import Input from "./Input";
 import { useAppSelector } from "./hooks";
 
@@ -13,20 +13,26 @@ export default function Header() {
 
     return (
         <header className="header">
-            <div>
+            <div className="header__left-wrapper">
                 <img className="header__logo" src={logo}></img>
                 <div className="header__search">
                     <FontAwesomeIcon icon={faMagnifyingGlass} />
                     <Input type="text" updateValue={() => {}} placeholder="Поиск курса"></Input>
                 </div>
             </div>
-
-            <Link to={"../profile"}><FontAwesomeIcon size="2x" icon={faUserCircle} />
-                <div className="header__user-wrapper">
-                    <span>{user.name}</span>
-                    <span className="header__user-wrapper-email">{user.email}</span>
+            <div className="header__right-wrapper">
+                <div className="header__notif-wrapper">
+                    <FontAwesomeIcon icon={faBell} />
+                    <span>3</span>
                 </div>
-            </Link>
+                <Link to={"../profile"}><FontAwesomeIcon size="2x" icon={faUserCircle} />
+                    <div className="header__user-wrapper">
+                        <span>{user.name}</span>
+                        <span className="header__user-wrapper-email">{user.email}</span>
+                    </div>
+                </Link>
+            </div>
+
         </header>
     )
 }
